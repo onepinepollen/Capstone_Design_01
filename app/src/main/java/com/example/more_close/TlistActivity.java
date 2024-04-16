5a8b16b2542dd281e93407c3bd4f1edadc81a99f
서진 장 <asj6558@naver.com>
2024-04-16 19:41:20 +0900

    Getting_closer ver.1.0

--- app/src/main/java/com/example/more_close/TlistActivity.java
+++ app/src/main/java/com/example/more_close/TlistActivity.java
@@ -1,15 +1,19 @@
 package com.example.more_close;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
@@ -24,6 +28,7 @@ public class TlistActivity extends AppCompatActivity {
         Intent intent = getIntent();
 
         LinearLayout scrollVL = findViewById(R.id.scrollViewLayout);
+
         Button btn1 = findViewById(R.id.button3);
         Button btn2 = findViewById(R.id.button);
 
@@ -66,17 +71,50 @@ public class TlistActivity extends AppCompatActivity {
 
                         // 다이얼로그를 닫습니다.
                         dialog.dismiss();
-                        if(name.isEmpty() || countStr.isEmpty()) {
-                        }
-                        else{
-                            // ScrollView에 새로운 TextView를 추가합니다.
-                            TextView textView = new TextView(TlistActivity.this);
-                            textView.setText("이름: " + name + ", 인원수: " + countStr);
-                            scrollVL.addView(textView);
+                        if (name.isEmpty() || countStr.isEmpty()) {
+                        } else {
+                            // LayoutInflater 인스턴스 생성
+                            LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                            // 추가할 XML 레이아웃을 인플레이트
+                            View team_btn = inflater.inflate(R.layout.team_button, null);
+                            // team_button 레이아웃을 스크롤 뷰에 추가합니다.
+                            TextView text1 = team_btn.findViewById(R.id.textView8);
+                            TextView text2 = team_btn.findViewById(R.id.textView10);
+                            ImageButton btn_D = team_btn.findViewById(R.id.imageButton);
+                            text1.setText(name);
+                            text2.setText(countStr);
+                            scrollVL.addView(team_btn);
+                            btn_D.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    AlertDialog.Builder builder1 = new AlertDialog.Builder(TlistActivity.this);
+                                    builder1.setTitle("정말로 삭제 하시겠습니까?"); // 다이얼로그 제목 설정
+                                    builder1.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialogInterface, int i) {
+                                            scrollVL.removeView(team_btn);
+                                        }
+                                    });
+                                    builder1.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialogInterface, int i) {
+                                        }
+                                    });
+                                    AlertDialog dialog = builder1.create();
+                                    builder1.show();
+                                }
+                            });
+                            team_btn.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    // 임시문구 나중에 해당 모임 화면으로 넘어갈 예정입니다.
+                                    Toast.makeText(TlistActivity.this, "모임 버튼이 클릭되었습니다.", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+
                         }
                     }
                 });
-
 
 
                 // 다이얼로그를 생성하고 표시합니다.
@@ -84,6 +122,7 @@ public class TlistActivity extends AppCompatActivity {
                 dialog.show();
             }
         });
+
 
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
