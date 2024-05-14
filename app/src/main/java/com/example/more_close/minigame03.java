c07153e996e90da6575ba21bf599f8b27d835dbe
one.pine.pollen <one.pine.pollen@gmail.com>
2024-05-14 22:31:55 +0900

    minigame 03,04 modify

--- app/src/main/java/com/example/more_close/minigame03.java
+++ app/src/main/java/com/example/more_close/minigame03.java
@@ -14,7 +14,13 @@ public class minigame03 extends AppCompatActivity {
     ImageView imageView;
     ImageView imageView2;
     ImageView imageView3;
-    ImageView imageView4; int index = 0;
+    ImageView imageView4;
+    ImageView imageView5;
+    ImageView imageView6;
+    ImageView imageView7;
+    ImageView imageView8;
+    ImageView imageView9;
+    int index = 0;
     Random random = new Random();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,12 @@ public class minigame03 extends AppCompatActivity {
         imageView2 = findViewById(R.id.imageView2);
         imageView3 = findViewById(R.id.imageView3);
         imageView4 = findViewById(R.id.imageView4);
+        imageView5 = findViewById(R.id.imageView5);
+        imageView6 = findViewById(R.id.imageView6);
+        imageView7 = findViewById(R.id.imageView7);
+        imageView8 = findViewById(R.id.imageView8);
+        imageView9 = findViewById(R.id.imageView9);
+
         btnTest = findViewById(R.id.btnTest);
         btnTest.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,25 +47,96 @@ public class minigame03 extends AppCompatActivity {
                         imageView2.setVisibility(View.INVISIBLE);
                         imageView3.setVisibility(View.INVISIBLE);
                         imageView4.setVisibility(View.INVISIBLE);
+                        imageView5.setVisibility(View.INVISIBLE);
+                        imageView6.setVisibility(View.INVISIBLE);
+                        imageView7.setVisibility(View.INVISIBLE);
+                        imageView8.setVisibility(View.INVISIBLE);
+                        imageView9.setVisibility(View.INVISIBLE);
+
                         break;
                     case 1:
                         imageView2.setVisibility(View.VISIBLE);
                         imageView.setVisibility(View.INVISIBLE);
                         imageView3.setVisibility(View.INVISIBLE);
                         imageView4.setVisibility(View.INVISIBLE);
+                        imageView5.setVisibility(View.INVISIBLE);
+                        imageView6.setVisibility(View.INVISIBLE);
+                        imageView7.setVisibility(View.INVISIBLE);
+                        imageView8.setVisibility(View.INVISIBLE);
+                        imageView9.setVisibility(View.INVISIBLE);
                         break;
                     case 2:
                         imageView3.setVisibility(View.VISIBLE);
                         imageView2.setVisibility(View.INVISIBLE);
                         imageView.setVisibility(View.INVISIBLE);
                         imageView4.setVisibility(View.INVISIBLE);
+                        imageView5.setVisibility(View.INVISIBLE);
+                        imageView6.setVisibility(View.INVISIBLE);
+                        imageView7.setVisibility(View.INVISIBLE);
+                        imageView8.setVisibility(View.INVISIBLE);
+                        imageView9.setVisibility(View.INVISIBLE);
                         break;
                     case 3:
                         imageView4.setVisibility(View.VISIBLE);
                         imageView2.setVisibility(View.INVISIBLE);
                         imageView3.setVisibility(View.INVISIBLE);
                         imageView.setVisibility(View.INVISIBLE);
+                        imageView5.setVisibility(View.INVISIBLE);
+                        imageView6.setVisibility(View.INVISIBLE);
+                        imageView7.setVisibility(View.INVISIBLE);
+                        imageView8.setVisibility(View.INVISIBLE);
+                        imageView9.setVisibility(View.INVISIBLE);
                         break;
+                    case 4:
+                        imageView.setVisibility(View.INVISIBLE);
+                        imageView2.setVisibility(View.INVISIBLE);
+                        imageView3.setVisibility(View.INVISIBLE);
+                        imageView4.setVisibility(View.INVISIBLE);
+                        imageView5.setVisibility(View.VISIBLE);
+                        imageView6.setVisibility(View.INVISIBLE);
+                        imageView7.setVisibility(View.INVISIBLE);
+                        imageView8.setVisibility(View.INVISIBLE);
+                        imageView9.setVisibility(View.INVISIBLE);
+                    case 5:
+                        imageView.setVisibility(View.INVISIBLE);
+                        imageView2.setVisibility(View.INVISIBLE);
+                        imageView3.setVisibility(View.INVISIBLE);
+                        imageView4.setVisibility(View.INVISIBLE);
+                        imageView5.setVisibility(View.INVISIBLE);
+                        imageView6.setVisibility(View.VISIBLE);
+                        imageView7.setVisibility(View.INVISIBLE);
+                        imageView8.setVisibility(View.INVISIBLE);
+                        imageView9.setVisibility(View.INVISIBLE);
+                    case 6:
+                        imageView.setVisibility(View.INVISIBLE);
+                        imageView2.setVisibility(View.INVISIBLE);
+                        imageView3.setVisibility(View.INVISIBLE);
+                        imageView4.setVisibility(View.INVISIBLE);
+                        imageView5.setVisibility(View.INVISIBLE);
+                        imageView6.setVisibility(View.INVISIBLE);
+                        imageView7.setVisibility(View.VISIBLE);
+                        imageView8.setVisibility(View.INVISIBLE);
+                        imageView9.setVisibility(View.INVISIBLE);
+                    case 7:
+                        imageView.setVisibility(View.INVISIBLE);
+                        imageView2.setVisibility(View.INVISIBLE);
+                        imageView3.setVisibility(View.INVISIBLE);
+                        imageView4.setVisibility(View.INVISIBLE);
+                        imageView5.setVisibility(View.INVISIBLE);
+                        imageView6.setVisibility(View.INVISIBLE);
+                        imageView7.setVisibility(View.INVISIBLE);
+                        imageView8.setVisibility(View.VISIBLE);
+                        imageView9.setVisibility(View.INVISIBLE);
+                    case 8:
+                        imageView.setVisibility(View.INVISIBLE);
+                        imageView2.setVisibility(View.INVISIBLE);
+                        imageView3.setVisibility(View.INVISIBLE);
+                        imageView4.setVisibility(View.INVISIBLE);
+                        imageView5.setVisibility(View.INVISIBLE);
+                        imageView6.setVisibility(View.INVISIBLE);
+                        imageView7.setVisibility(View.INVISIBLE);
+                        imageView8.setVisibility(View.INVISIBLE);
+                        imageView9.setVisibility(View.VISIBLE);
                 }
             }
         });
