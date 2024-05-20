7024752f39a9dab4349784b315c38294ad0b388e
one.pine.pollen <one.pine.pollen@gmail.com>
2024-05-21 00:51:51 +0900

    intro / logo

---
+++ app/src/main/java/com/example/more_close/intro_main.java
@@ -0,0 +1,30 @@
+package com.example.more_close;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class intro_main extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intro_main); //xml , java 소스 연결
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                // 인트로 화면을 보여준 후 intent를 사용해 메인화면인 MainActivity.class로 넘어가도록 함
+                Intent intent = new Intent (getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000); //2초 뒤에 Runner객체 실행하도록 함
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        finish();
+    }
+}
